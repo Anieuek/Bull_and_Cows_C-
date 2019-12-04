384a60df2174e246704a9e1c8c29361963b5b77a
AnieuekEunieuek <aniak127@interia.pl>
2019-12-04 19:25:17 +0100

    The Second step

--- Solution/Bull_and_Cow/Main.cpp
+++ Solution/Bull_and_Cow/Main.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 #include <string>
 
+
+void PrintIntro();
+std::string GetGuessAndPrintBack();
+
+//the entry point for my application
 int main()
 {
-	// introduce the game
+	PrintIntro();
+
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+	
+	std::cout << std::endl;
+	return 0;
+}
+
+// introduce the game
+void PrintIntro() {
 	constexpr int WORLD_LENGTH = 5;
 	std::cout << "Welcome to Bulls and Cows, a fun word game\n";
 	std::cout << "Can you guess the " << WORLD_LENGTH;
 	std::cout << " letter isogram I'm thinking of?\n";
 	std::cout << std::endl;
-
-	// get a guess from the player
+	return;
+}
+// get a guess from the player
+std::string GetGuessAndPrintBack() {
 	std::cout << "Enter your guess: ";
 	std::string Guess = "";
-	std::cin >> Guess;
-
-	// repeat the guess back to them
-	std::cout << "Your guess was " << Guess << std::endl;
-
-	// get a guess from the player
-	std::cout << "Enter your guess: ";
-	std::cin >> Guess;
-
-	// repeat the guess back to them
-	std::cout << "Your guess was " << Guess << std::endl;
+	std::getline(std::cin, Guess);
 	
-	std::cout << std::endl;
-	return 0;
+	// print the guess back
+	std::cout << "Your guess was " << Guess << std::endl;
+	return Guess;
 }
